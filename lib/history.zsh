3593232e6a565c0470263abf89e999ae676522c3
J.D. Fagan <jd.fagan@gmail.com>
2020-04-24 08:19:20 -0700

    Set my preferred history behavior (do not share .zsh_history across concurrent shells until after session closes).  See https://github.com/ohmyzsh/ohmyzsh/issues/6909

--- lib/history.zsh
+++ lib/history.zsh
@@ -27,8 +27,8 @@ esac
 
 ## History file configuration
 [ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
-HISTSIZE=50000
-SAVEHIST=10000
+HISTSIZE=1000000000
+SAVEHIST=1000000000
 
 ## History command configuration
 setopt extended_history       # record timestamp of command in HISTFILE
@@ -36,5 +36,8 @@ setopt hist_expire_dups_first # delete duplicates first when HISTFILE size excee
 setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
 setopt hist_verify            # show command with history expansion to user before running it
-setopt inc_append_history     # add commands to HISTFILE in order of execution
-setopt share_history          # share command history data
+
+# Mutually exclusively set between the following 3 settings
+setopt inc_append_history_time  # separate concurrent histories across shells until session closes
+unsetopt inc_append_history     # add commands to HISTFILE in order of execution
+unsetopt share_history          # share command history data
